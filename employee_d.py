7556727103a2bc61e5b5e73cf9756a6ccfa896e2
keerthimellihalli <keerthimellihalli@gmail.com>
2025-12-07 19:56:06 +0530

    Update employee_d.py

--- employee_d.py
+++ employee_d.py
@@ -1,17 +1,20 @@
 def employee_details(name, emp_id, department, salary):
     return {
-        "employee_name": name + "\n",
-        "employee_id": emp_id + "\n",
-        "department": department + "\n",
-        "salary": str(salary) + "\n"
+        "employee_name": name,
+        "employee_id": emp_id,
+        "department": department,
+        "salary": (salary),
     }
 
 if __name__ == "__main__":
     # Sample input
-    name = "John Doe"
-    empl_id = "E12345"
-    department = "Engineering"
-    salary = 75000
+    {
+  'employee_name': 'John Doe\n',
+  'employee_id': 'E12345\n',
+  'department': 'Engineering\n',
+  'salary': '75000\n'
+}
+
 
     details = employee_details(name, empl_id, department, salary)
     print(details)
